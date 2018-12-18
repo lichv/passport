482117bd20b4edfe0a0fa308863910cd35383c4a
lichv <lichvg@126.com>
2018-12-18 20:00:15 +0800

    modify database migrate

--- database/migrations/2016_06_01_000004_create_oauth_clients_table.php
+++ database/migrations/2016_06_01_000004_create_oauth_clients_table.php
@@ -16,12 +16,14 @@ class CreateOauthClientsTable extends Migration
         Schema::create('oauth_clients', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->index()->nullable();
-            $table->string('name');
+            $table->string('name', 100);
+            $table->string('key', 100);
             $table->string('secret', 100);
             $table->text('redirect');
             $table->boolean('personal_access_client');
             $table->boolean('password_client');
             $table->boolean('revoked');
+            $table->boolean('silence');
             $table->timestamps();
         });
     }
